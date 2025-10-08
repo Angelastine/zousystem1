366675b6631452f133e656aa8c5f8f3cae5dcc3b
Angelastine <mazhindutalent@gmail.com>
2025-10-08 11:02:16 +0200

    tede

--- lms/student_dash/urls.py
+++ lms/student_dash/urls.py
@@ -9,5 +9,4 @@ urlpatterns = [
     path('book/<int:pk>/', views.book_detail, name='book_detail'),
     path('book/<int:pk>/download/', views.download_book, name='download_book'),
     path('borrow/<int:pk>/', views.borrow_book, name='borrow_book'),
-
 ]
